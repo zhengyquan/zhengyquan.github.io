85638535783a4d73a33224e92a0eca607fa31061
zhengyongquan <2750932784@qq.com>
2023-04-09 16:54:29 +0800

    Site updated: 2023-04-09 16:54:28

--- js/rightmenu.js
+++ js/rightmenu.js
@@ -15,20 +15,22 @@ rmf.showRightMenu = function(isTrue, x=0, y=0){
 
 // 昼夜切换
 rmf.switchDarkMode = function(){
-    const nowMode = document.documentElement.getAttribute('data-theme') === 'dark' ? 'dark' : 'light'
-    if (nowMode === 'light') {
-        activateDarkMode()
-        saveToLocal.set('theme', 'dark', 2)
-        GLOBAL_CONFIG.Snackbar !== undefined && btf.snackbarShow(GLOBAL_CONFIG.Snackbar.day_to_night)
-    } else {
-        activateLightMode()
-        saveToLocal.set('theme', 'light', 2)
-        GLOBAL_CONFIG.Snackbar !== undefined && btf.snackbarShow(GLOBAL_CONFIG.Snackbar.night_to_day)
-    }
-    // handle some cases
-    typeof utterancesTheme === 'function' && utterancesTheme()
-    typeof FB === 'object' && window.loadFBComment()
-    window.DISQUS && document.getElementById('disqus_thread').children.length && setTimeout(() => window.disqusReset(), 200)
+    // const nowMode = document.documentElement.getAttribute('data-theme') === 'dark' ? 'dark' : 'light'
+    // if (nowMode === 'light') {
+    //     activateDarkMode()
+    //     saveToLocal.set('theme', 'dark', 2)
+    //     GLOBAL_CONFIG.Snackbar !== undefined && btf.snackbarShow(GLOBAL_CONFIG.Snackbar.day_to_night)
+    // } else {
+    //     activateLightMode()
+    //     saveToLocal.set('theme', 'light', 2)
+    //     GLOBAL_CONFIG.Snackbar !== undefined && btf.snackbarShow(GLOBAL_CONFIG.Snackbar.night_to_day)
+    // }
+    // // handle some cases
+    // typeof utterancesTheme === 'function' && utterancesTheme()
+    // typeof FB === 'object' && window.loadFBComment()
+    // window.DISQUS && document.getElementById('disqus_thread').children.length && setTimeout(() => window.disqusReset(), 200)
+    // 昼夜切换动画
+    switchNightMode()
 };
 
 // 阅读模式
